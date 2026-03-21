e7539f87790b8f006c7bd0f68aad972b4f819e08
Bozhidar Enchev <bojidar.enchev03@gmail.com>
2026-03-21 16:32:45 +0200

    feat: register models to admin dashboard

--- FinanceTracker/finances/admin.py
+++ FinanceTracker/finances/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from FinanceTracker.finances.models import Transaction, TransactionCategory, FinancialAccount, Target
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TransactionCategory)
+class TransactionCategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Target)
+class TargetAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(FinancialAccount)
+class FinancialAccountAdmin(admin.ModelAdmin):
+    pass
