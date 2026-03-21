4ae44f4279a7aba2a2076ff2fcb2dc2d34129bb0
Bozhidar Enchev <bojidar.enchev03@gmail.com>
2026-03-21 22:52:05 +0200

    feat: rename ForeignKey fields and add related names

--- FinanceTracker/finances/models.py
+++ FinanceTracker/finances/models.py
@@ -6,17 +6,19 @@ from FinanceTracker.common.mixins import BaseMixin
 
 
 class Transaction(models.Model, BaseMixin):
-    user_id = models.ForeignKey(
+    user = models.ForeignKey(
         'accounts.AppUser',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='transactions',
     )
     amount = models.DecimalField(
         decimal_places=2,
         max_digits=10
     )
-    category_id = models.ForeignKey(
+    category = models.ForeignKey(
         'TransactionCategory',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='transactions',
     )
     date = models.DateField(
         default=datetime.date.today
@@ -33,9 +35,10 @@ class TransactionCategory(models.Model, BaseMixin):
     name = models.CharField(
         max_length=100,
     )
-    user_id = models.ForeignKey(
+    user = models.ForeignKey(
         'accounts.AppUser',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='transaction_categories'
     )
     is_need = models.BooleanField(
         default=False,
@@ -72,9 +75,10 @@ class FinancialAccount(models.Model, BaseMixin):
     name = models.CharField(
         max_length=100,
     )
-    user_id = models.ForeignKey(
+    user = models.ForeignKey(
         'accounts.AppUser',
         on_delete=models.CASCADE,
+        related_name='financial_accounts',
     )
     amount = models.DecimalField(
         decimal_places=2,
