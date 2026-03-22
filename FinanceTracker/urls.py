e184e7a1c8a3315ee8a74dd39086c9321d8e931c
Bozhidar Enchev <bojidar.enchev03@gmail.com>
2026-03-22 18:30:53 +0200

    feat: remove raw schema url

--- FinanceTracker/urls.py
+++ FinanceTracker/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('FinanceTracker.finances.urls')),
     path('', include('FinanceTracker.accounts.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
