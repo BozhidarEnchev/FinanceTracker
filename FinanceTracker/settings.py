4af72696949f66f4f1cbf63976e7b726b155aae9
Bozhidar Enchev <bojidar.enchev03@gmail.com>
2026-03-22 02:11:26 +0200

    feat: install drf_spectatcular and setup swagger

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -47,7 +47,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
 ] + MY_APPS
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Finance Tracker',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
